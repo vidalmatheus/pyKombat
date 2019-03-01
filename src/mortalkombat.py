89654e034878d40aed9b2c1f520f63aef678eb94
vidalmatheus <matheusvidaldemenezes@gmail.com>
2019-03-01 00:25:50 -0300

    up

--- src/mortalkombat.py
+++ src/mortalkombat.py
@@ -1,5 +1,7 @@
-import pygame, os
+import pygame
+import os
 import config           # importa config.py
 import menu             # importa menu.py
-from game import Point  # importa apenas a classe Point de game.py
+# from game import Point  # importa apenas a classe Point de game.py
+
 
