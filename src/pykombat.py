18ce96b1683143bc6ec3d3dc03c846a6b3991579
vidalmatheus <matheusvidaldemenezes@gmail.com>
2019-03-01 11:02:20 -0300

    Presentation

--- src/pykombat.py
+++ src/pykombat.py
@@ -4,4 +4,6 @@ import config           # importa config.py
 import menu             # importa menu.py
 # from game import Point  # importa apenas a classe Point de game.py
 
-
+if _name_ == "main":
+    printf('loading...')
+    
