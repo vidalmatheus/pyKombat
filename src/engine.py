bbcc089b5aef24cdb98ae9aef1685402665311af
alvesouza <36734373+alvesouza@users.noreply.github.com>
2019-03-22 04:40:47 -0300

    .

--- src/engine.py
+++ src/engine.py
@@ -339,16 +339,10 @@ class Transform:
 
 class GameObject:
 
-<<<<<<< HEAD
-    def __init__(self,file,sprite_width, sprite_height, name="object", vec=Vector2(0.0, 0.0)):
-        self.name = name
-        self.sprite_list = SpriteSheetLoader(file, sprite_width, sprite_height).getSpriteList()
-=======
     def __init__(self, name="object", vec=Vector2(0.0, 0.0),gameScene = GameScene()):
         self.name = name
         self.gameScene = gameScene
         self.layer = 0
->>>>>>> Pedro
         self.transform = Transform(vec)
         self.maxlength = self.getmaxlength()
         self.tick = 0
