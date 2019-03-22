910cb167113dd08393fe98b4c02a3579de880cd9
vidalmatheus <matheusvidaldemenezes@gmail.com>
2019-03-21 22:17:04 -0300

    up

--- src/pykombat.py
+++ src/pykombat.py
@@ -3,10 +3,10 @@ import os
 from pygame.locals import *
 import config           # importa config.py
 import menu             # importa menu.py
-import gameState        # importa gameState.py
+import engine           # importa engine.py
 # from game import Point  # importa apenas a classe Point de game.py
 
 if __name__ == "__main__":
     print('loading...')
 
-    gameState.gameStateManager()
+    
