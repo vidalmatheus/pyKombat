d58a1b4457996eda7b3d7afe716becfa25186f9f
sadrianorod <sadrianorod@gmail.com>
2019-03-22 00:28:34 -0300

    fighter foi iniciado

--- src/fighter.py
+++ src/fighter.py
@@ -1,5 +1,19 @@
-import Engine
+import engine
 
 """Usa a classe GameObject como superClasse e veja as classes Animation,AnimationManager e Transform"""
 """criado um condigo pra facilitar os testes renderizados,então pra testar vai ter que criar vc mesmo,
 usar a classe Game e Camera pode ajudar"""
+
+class Fighter(engine.GameObject):
+    def __init__(self, name="object", vec=Vector2(0.0, 0.0)):
+        super().__init__(name,vec)
+        self.fighterName = name
+        self.position = vec
+        #self.loadImages = #Tem que upar as imagens do Scorpion
+         
+    
+    def initializeScorpion(self,position,game):
+        engine.ImageManager.drawImage(image, self.position, game)        
+
+
+
